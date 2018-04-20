e16386dcd07bca6d600215facd2d41605167a645
romain Focone <email@example.com>
2018-04-20 12:20:21 +0200

    add splashscreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import {ModalController, Platform} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 
 import { TabsPage } from '../pages/tabs/tabs';
+import {SplashPage} from "../pages/splash/splash";
 
 @Component({
   templateUrl: 'app.html'
@@ -12,12 +13,14 @@ import { TabsPage } from '../pages/tabs/tabs';
 export class MyApp {
   rootPage:any = TabsPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,modalCtrl:ModalController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-      splashScreen.hide();
+        let splash = modalCtrl.create(SplashPage);
+        splash.present();
+      //splashScreen.hide();
     });
   }
 }
